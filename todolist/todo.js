bd3aebba1df875aa1e71c25a05cca9d97d8f8ec3
Sabirhussen shaikh <shaikhsabirhussen@gmail.com>
2024-10-14 21:19:23 +0530

    make todoList array and display addTodo function in console

---
+++ todolist/todo.js
@@ -0,0 +1,13 @@
+// todo array
+const todoList = [];
+
+function addTodo() {
+    
+    const inputElement = document.querySelector('.js-input-name');
+    const name = inputElement.value;
+
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value = '';
+}
