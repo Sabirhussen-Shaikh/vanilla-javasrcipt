4abd2ca9fe8416a5202e078846eecbdf7728e6e2
Sabirhussen shaikh <shaikhsabirhussen@gmail.com>
2024-10-15 19:38:14 +0530

    render function and use of array.splice() function to delete todo

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,13 +1,42 @@
 // todo array
-const todoList = [];
+const todoList = ['study', 'assignment', 'sports'];
 
+renderTodoList();
+
+//render function
+function renderTodoList() {
+
+    let todoHtml = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+        const html = `
+        <p>
+            ${todo} 
+            <button onclick="
+                todoList.splice(${i},1);
+                renderTodoList();
+            ">Delete</button>
+        </p>
+        `;
+
+        todoHtml += html;
+    }
+    console.log(todoHtml)
+
+    document.querySelector('.js-todo-render').innerHTML = todoHtml;
+}
+
+//add function
 function addTodo() {
-    
+
     const inputElement = document.querySelector('.js-input-name');
     const name = inputElement.value;
 
     todoList.push(name);
-    console.log(todoList);
+    // console.log(todoList);
 
     inputElement.value = '';
+
+    renderTodoList();
 }
