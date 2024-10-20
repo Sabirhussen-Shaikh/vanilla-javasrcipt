7af6c2969881f72040b0e89836a40032c5a96671
Sabirhussen shaikh <shaikhsabirhussen@gmail.com>
2024-10-20 19:12:47 +0530

    basics of todo

---
+++ todolist_with_chartJS/todoChart.js
@@ -0,0 +1,108 @@
+// todo array
+// const todoList = [{
+//     name : 'study',
+//     dueDate : '22-03-2024'
+// },{
+//     name : 'assignment',
+//     dueDate : '08-05-2022'
+// },{
+//     name : 'sports',
+//     dueDate : '30-10-2024'
+// }];
+
+const todoList = JSON.parse(localStorage.getItem('todoValues'));
+
+console.log(JSON.parse(localStorage.getItem('todoValues')));
+
+renderTodoList();
+
+//render function
+function renderTodoList() {
+
+    let todoHtml = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObj = todoList[i];
+
+        // const name = todoObj.name;
+        // const dueDate = todoObj.dueDate;
+        const { name, dueDate } = todoObj;//destructing
+
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button class = "delete-btn js-delete-btn"
+        ">Delete</button>
+        `;
+
+        todoHtml += html;
+
+    }
+    // console.log(todoHtml)
+
+    localStorage.setItem('todoValues', JSON.stringify(todoList));
+
+    document.querySelector('.js-todo-render').innerHTML = todoHtml;
+
+    //delete todo function()
+    const deleteBtns = document.querySelectorAll('.js-delete-btn');
+
+    deleteBtns.forEach((deleteButton , index) => {
+        deleteButton.addEventListener('click' , () => {
+            todoList.splice(index,1);
+            renderTodoList();
+        })
+    });
+
+}
+
+//add function
+function addTodo() {
+
+    if (isEmpty() == false) {
+
+        // querySelector
+        const inputElement = document.querySelector('.js-input-name');
+        const nameEntered = inputElement.value;
+
+        const dateInputElement = document.querySelector('.js-input-date');
+        const dueDateEntered = dateInputElement.value;
+
+        todoList.push({
+            name: nameEntered,
+            dueDate: dueDateEntered
+            //shorthand property
+            // name,
+            // dueDate
+        });
+        console.log(todoList);
+
+        inputElement.value = '';
+        dateInputElement.value = '';
+
+        renderTodoList();
+    }
+
+}
+
+function isEmpty() {
+
+    // querySelector
+    const inputElement = document.querySelector('.js-input-name');
+    const nameEntered = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-input-date');
+    const dueDateEntered = dateInputElement.value;
+
+    if (nameEntered === '') {
+        alert("please enter todo name...!");
+        return true;
+    }
+
+    if (dueDateEntered === '') {
+        alert('please enter deadline date...!');
+        return true;
+    }
+
+    return false;
+}
